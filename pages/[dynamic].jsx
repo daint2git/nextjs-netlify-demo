b159f47b75aa2d0f2baebd2323b6b585dfa2d01a
daint2git <daint2dev@gmail.com>
2021-04-21 16:33:19 +0700

    test

--- pages/[dynamic].jsx
+++ pages/[dynamic].jsx
@@ -1,11 +1,18 @@
 import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Dynamic = () => {
   const router = useRouter();
+  const { t } = useTranslation("common");
   const { dynamic } = router.query;
 
-  return <p>Dynamic: {dynamic}</p>;
+  return (
+    <div>
+      <p>{`Dynamic: ${dynamic}`}</p>
+      <p>{t("error")}</p>
+    </div>
+  );
 };
 
 export const getServerSideProps = async ({ locale }) => ({
