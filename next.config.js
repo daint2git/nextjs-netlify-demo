5a4fc43eccf44052a580c665ec172cb48015a948
daint2git <daint2dev@gmail.com>
2021-04-21 22:52:22 +0700

    test

--- next.config.js
+++ next.config.js
@@ -3,4 +3,24 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   target: "experimental-serverless-trace",
   i18n,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.jsx?$/,
+      use: [
+        {
+          loader: require.resolve("@svgr/webpack"),
+        },
+        {
+          loader: require.resolve("url-loader"),
+          options: {
+            name: "[name].[hash:8].[ext]",
+            limit: 10240,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
+import logo, { ReactComponent as Logo } from "../public/images/logo.svg";
+
 export default function Index() {
   return (
     <div>
       <h1>Index</h1>
       <img src="images/logo192.png" alt="logo" />
+      <img src="images/logo.svg" alt="logo" />
+      <img src={logo} alt="logo" />
+      <Logo />
     </div>
   );
 }
