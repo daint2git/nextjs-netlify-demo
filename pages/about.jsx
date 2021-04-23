54b5ddb1998504b5ed2e5c6360c84c06070fef72
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2021-04-23 13:20:31 +0700

    test

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,17 +5,20 @@ function About({ x }) {
     <>
       <Head>
         <link rel="icon" href="images/favicon.ico" />
+        <link rel="apple-touch-icon" href="images/logo192.png" />
         <title>dai title</title>
         <meta property="description" content="dai description" />
+        
         <meta property="og:title" content="dai og title" />
         <meta property="og:description" content="dai og description" />
         <meta property="og:image" content="images/og-image.jpeg" />
-        <meta property="og:site_name" content="DaiNetlify" />
-        <meta property="og:locale" content="vi" />
         <meta
           property="og:url"
           content="https://github.com/daint2git/nextjs-netlify-demo"
         />
+
+        <meta property="og:site_name" content="DaiNetlify" />
+        <meta property="og:locale" content="vi" />
         <meta property="og:type" content="website" />
       </Head>
       <div>
