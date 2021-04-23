7d033ffe95150831a748ef0358bdeeca0956d0dd
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2021-04-23 13:05:37 +0700

    test

--- pages/about.jsx
+++ pages/about.jsx
@@ -6,15 +6,14 @@ function About({ x }) {
       <Head>
         <link rel="icon" href="images/favicon.ico" />
         <title>dai title</title>
-        <meta name="description" content="dai description" />
+        <meta property="description" content="dai description" />
         <meta property="og:title" content="dai og title" />
         <meta property="og:description" content="dai og description" />
+        <meta property="og:image" content="images/og-image.jpeg" />
+        <meta property="og:site_name" content="DaiNetlify" />
+        <meta property="og:locale" content="vi" />
         <meta
-          name="og:image"
-          content="https://i.picsum.photos/id/372/200/200.jpg?hmac=QFGGlcWGNWBK0oDD1jghIaCvGIFU5iJJcd2VhF5oH6o"
-        />
-        <meta
-          name="og:url"
+          property="og:url"
           content="https://github.com/daint2git/nextjs-netlify-demo"
         />
         <meta property="og:type" content="website" />
