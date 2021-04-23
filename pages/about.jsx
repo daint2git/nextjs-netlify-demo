29738d873ea076ce754c595604e5cbd1a7de50af
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2021-04-23 12:53:43 +0700

    test

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,15 +4,20 @@ function About({ x }) {
   return (
     <>
       <Head>
+        <link rel="icon" href="images/favicon.ico" />
         <title>dai title</title>
         <meta name="description" content="dai description" />
         <meta property="og:title" content="dai og title" />
         <meta property="og:description" content="dai og description" />
-        <meta name="og:image" content="https://picsum.photos/200" />
+        <meta
+          name="og:image"
+          content="https://i.picsum.photos/id/372/200/200.jpg?hmac=QFGGlcWGNWBK0oDD1jghIaCvGIFU5iJJcd2VhF5oH6o"
+        />
         <meta
           name="og:url"
           content="https://github.com/daint2git/nextjs-netlify-demo"
         />
+        <meta property="og:type" content="website" />
       </Head>
       <div>
         <h1>About</h1>
